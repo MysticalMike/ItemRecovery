2a42ed63b148ef6cb6320c28621eb8239eaf784e
MysticalMike <MysticalMike@users.noreply.github.com>
2021-09-29 18:33:53 -0600

    Removed test comment

--- ItemRecovery/ModEntry.cs
+++ ItemRecovery/ModEntry.cs
@@ -9,7 +9,6 @@ namespace ItemRecovery
         public static ModConfig Config;
         private static IMonitor _monitor;
         
-        // test
         public override void Entry(IModHelper helper)
         {
             Config = helper.ReadConfig<ModConfig>();
